2280d2822bbfb84ebd456334e54e6fcbfd5745c3
caicodes <caicodes1@gmail.com>
2022-06-29 22:38:50 -0600

    fixes

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,7 +4,7 @@ import Logo from "../components/Logo";
 const Home = () => {
   return (
     <>
-      <div className="grid h-screen place-items-center bg-blue-300 dark:bg-blue-900 text-white fixed w-full">
+      <div className="grid h-screen place-items-center bg-blue-300 dark:bg-blue-900 text-white">
         <div className="flex text-3xl text-center bg-blue-100  font-bold text-blue-500 dark:text-blue-400 dark:bg-blue-700 p-4 rounded md:mb-80 mb-40 transition-all duration-300">
           <Logo />
         </div>
